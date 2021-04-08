eb9de70ee4dfb5f3efbd2dea55b0fd039cbe2089
Rafif <hanifrafif22@gmail.com>
2021-04-08 18:25:09 +0700

    Modul 5 - Case Study - Program, Regex

--- JWork.java
+++ JWork.java
@@ -13,12 +13,9 @@ public class JWork
     }
     
     public static void main(String[] args) {
-        Jobseeker jobseeker1 = new Jobseeker(1, "Zufar", "hanif.zufar@ui.ac.id", "HanifZufar", new GregorianCalendar(2000, 7, 22));
+        Jobseeker jobseeker1 = new Jobseeker(1, "Zufar", "hanif.zufar@ui.ac.id", "Hanif123", new GregorianCalendar(2000, 7, 22));
         Jobseeker jobseeker2 = new Jobseeker(2, "Zufar", "hanif.zufar@ui.ac.id", "Hanif123", 2000, 3, 28);
         Jobseeker jobseeker3 = new Jobseeker(3, "Zufar", "hanif.zufar@ui.ac.id", "Hanif123");
         System.out.println(jobseeker1.toString());
-        System.out.println(jobseeker2.toString());
-        System.out.println(jobseeker3.toString());
-    }
-        
+    }   
 }
